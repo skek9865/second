c1337cbfc7fd01a7fe5927068a8779a71f3c121e
skek <skek9865>
2021-08-15 18:22:27 +0900

    hi

---
+++ team/first/src/test/java/com/with/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.with.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
